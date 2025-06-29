dc4f3b516f95eebf7106956a2339b341916ef1cc
OCP <oscarcarballopuebla@gmail.com>
2025-06-29 02:02:08 +0200

    iluminacion

---
+++ hypr/scripts/brightness.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BRIGHTNESS_DEVICE="intel_backlight"
+
+case "$1" in
+  up)
+    brightnessctl set +1% > /dev/null
+  ;;
+down)
+    brightnessctl set 1%- > /dev/null
+  ;;
+get)
+   BRIGHTNESS=$(brightnessctl -d "$BRIGHTNESS_DEVICE" get)
+   MAX_BRIGHTNESS=$(brightnessctl -d "$BRIGHTNESS_DEVICE" max)
+   PERCENTAGE=$((BRIGHTNESS * 100 / MAX_BRIGHTNESS))
+   echo "{\"text\": \"󰌵 $PERCENTAGE%\", \"class\": \"brightness\"}"
+  ;;
+*)
+  echo "Uso: $0 [up|down|get]"
+  exit 1
+  ;;
+esac
