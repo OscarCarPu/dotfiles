2e86a4d6a1cd834719376a4fca23944d17e90f32
OCP <oscarcarballopuebla@gmail.com>
2025-08-13 23:30:37 +0200

    astronvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,98 +1,27 @@
--- Basic settings
-vim.opt.number = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.g.mapleader = " "
-vim.opt.clipboard = "unnamedplus"
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
--- lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  local result = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  if vim.v.shell_error ~= 0 then
+    -- stylua: ignore
+    vim.api.nvim_echo({ { ("Error cloning lazy.nvim:\n%s\n"):format(result), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+  end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
--- lazy.nvim setup
-require("lazy").setup({
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate"
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" }
-  },
-  { "github/copilot.vim" }
-})
-
--- Catppuccin
-require("catppuccin").setup({
-  flavour = "mocha",
-  transparent_background = false
-})
-vim.cmd [[colorscheme catppuccin]]
-
--- Treesitter setup
-require("nvim-treesitter.configs").setup {
-  ensure_installed = { "lua", "python", "javascript", "cpp", "bash", "html", "css", "json", "yaml", "sql" },
-  highlight = {
-    enable = true
-  }
-}
-
--- Telescope setup
-local telescope = require("telescope.builtin")
-vim.keymap.set("n","<leader>ff", telescope.find_files, {})
-vim.keymap.set("n","<leader>fg",telescope.live_grep, {}) -- Seach text
-vim.keymap.set("n","<leader>fb",telescope.buffers, {})
-
--- Nvim Tree
-require("nvim-tree").setup({
-  view = { width = 30 },
-  filters = {
-    dotfiles = false,
-    git_ignored = false
-  },
-  git = {
-    enable = true,
-    ignore = false
-  },
-  renderer = { icons = { show = {
-    file = true,
-    folder = true,
-    folder_arrow = true,
-    git = true
-  }}}
-})
-
-vim.keymap.set("n","<leader>e",":NvimTreeToggle<CR>", {})
-
--- Copilot setup
-vim.g.copilot_no_tab_map = true
-local map = function (type, key, value, opts)
-  local options = { noremap = true, silent = true }
-  if opts then options = vim.tbl_extend("force", options, opts) end
-  vim.api.nvim_set_keymap(type, key, value, options)
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
 end
-map("i", "<C-j>", 'copilot#Accept("<CR>")', { expr = true, silent = true, expr=true })
 
-
+require "lazy_setup"
+require "polish"
