e6947f7425611ba7c212018c18d54213c1f29f9a
OCP <oscarcarballopuebla@gmail.com>
2025-09-17 19:29:48 +0200

    hyprland shutdwon with update

---
+++ hypr/scripts/update_and_shutdown.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run system updates in a new Kitty terminal window
+kitty --title "System Update" -e bash -c "
+echo 'Running system updates...';
+sudo pacman -Syu;
+echo;
+yay -Syu;
+echo;
+read -p 'Updates complete. Press Enter to shutdown or close this window to cancel.';
+systemctl poweroff
+"
