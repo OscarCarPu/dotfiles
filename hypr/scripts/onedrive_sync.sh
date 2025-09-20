ab4da651fde611c7609715e4c19aab631b3873a6
OCP <oscarcarballopuebla@gmail.com>
2025-09-20 18:28:33 +0200

    onedrive

---
+++ hypr/scripts/onedrive_sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+onedrive -s &
+
+
--- hypr/scripts/shutdown.sh
+++ hypr/scripts/shutdown.sh
@@ -2,6 +2,8 @@
 
 # Run system updates in a new Kitty terminal window
 kitty --title "System Update" -e bash -c "
+echo 'Syncing onedrive';
+onedrive -s --verbose;
 echo 'Running system updates...';
 sudo pacman -Syu;
 echo;
