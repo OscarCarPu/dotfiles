636d497e85f9751d2e641945b9d97a62f15025dd
OCP <oscarcarballopuebla@gmail.com>
2025-09-25 00:58:55 +0200

    remove nvim

--- nvim/lua/polish.lua
+++
@@ -1,8 +0,0 @@
--- This will run last in the setup process.
--- This is just pure lua so anything that doesn't
--- fit in the normal config locations above can go here
---
-vim.opt.wrap = true
-vim.opt.linebreak = true
-vim.opt.breakindent = true
-vim.opt.showbreak = "↪ "
