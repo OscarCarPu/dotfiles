d451499f0c8f1e4465924785bf5cbe91dfd48ba7
OCP <oscarcarballopuebla@gmail.com>
2025-12-02 11:56:02 +0100

    some conf

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,19 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
---
+
+-- Enable system clipboard integration (uses wl-clipboard on Wayland)
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
