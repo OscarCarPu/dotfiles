4bfd143629729451c96a3795044f44bf686b8103
OCP <oscarcarballopuebla@gmail.com>
2026-02-01 00:04:37 +0100

    refactor: simplify nvim configuration and update license
    * remove redundant configurations and plugins
    * update LICENSE to a more concise version
    * add .stylua.toml for code formatting
    * refactor lua scripts for better organization and readability
    * remove unused files and configurations

---
+++ nvim/lua/options.lua
@@ -0,0 +1,12 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt ='both'
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
+o.expandtab = true
+o.wrap = true
+o.linebreak = true
