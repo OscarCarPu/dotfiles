beb10b4806487e3d5769e5b511ceb022f4aa1abe
OCP <oscarcarballopuebla@gmail.com>
2026-02-20 14:42:40 +0100

    feat: add Android, Java, and Kotlin support to Nvim * Configure Android plugin for building, running, and viewing Logcat * Add Java LSP support via jdtls * Add Kotlin LSP support via kotlin-language-server * Add XML LSP support via lemminx * Install required tools via Mason * Update Treesitter for syntax highlighting * Enhance plugin configuration for Java, Kotlin, and XML files

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,9 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls", "rust_analyzer" }
+local servers = { "html", "cssls", "gopls", "rust_analyzer", "kotlin_ls", "lemminx" }
 vim.lsp.enable(servers)
 
 require "configs.go"
 require "configs.rust"
+require "configs.kotlin"
+require "configs.xml"
 
 -- read :h vim.lsp.config for changing options of lsp servers
