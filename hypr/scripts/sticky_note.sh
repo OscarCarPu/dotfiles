37c1e929663dd1d069d2003f39fb7aae907f316c
OCP <oscarcarballopuebla@gmail.com>
2026-02-26 11:49:51 +0100

    feat: enhance sticky notes functionality
    * modify sticky_note.sh to use vim for editing notes with custom settings
    * add sticky_note.vim to configure vim settings for notes
    * update kitty.conf to set font family and disable os window close confirmation

--- hypr/scripts/sticky_note.sh
+++ hypr/scripts/sticky_note.sh
@@ -7,5 +7,9 @@ NOTES_FILE="$HOME/.sticky-notes.md"
 if hyprctl clients -j | grep -q '"class": "sticky-notes"'; then
     hyprctl dispatch closewindow class:sticky-notes
 else
-    kitty --class sticky-notes -e micro "$NOTES_FILE"
+    touch "$NOTES_FILE"
+    kitty \
+        --class sticky-notes \
+        --override window_padding_width=16 \
+        -e vim -c "source $HOME/.dotfiles/hypr/scripts/sticky_note.vim" "$NOTES_FILE"
 fi
